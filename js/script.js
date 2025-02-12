bf862b2024b26ed9c0d68ef7034e654678772621
Yama Roni <yamaroni8@gmail.com>
2025-02-12 20:13:01 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // Ini file Javascript
+
+console.log('Javascript is working!');
+
+function validateForm() {
+    let input = document.getElementById('main-input');
+    console.log(input.value);
+
+    if (input.value == '') {
+        alert('Tolong isi inputan');
+    } else {
+        /// Proses konversi
+        let calc = convertToCelcius(input.value);
+
+        /// Tampilkan hasil
+        document.getElementById('main-result').value = calc;
+        document.getElementById('cara-konversi').value = `Diket: C=${input.value} F...`;
+        console.log('Berhasil di eksekusi');
+    }
+}
+
+/// Fungsi konversi
+let convertToCelcius = (input) => {
+    return input * 2;
+}
